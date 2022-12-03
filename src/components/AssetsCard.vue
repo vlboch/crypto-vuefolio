a8f3b5cb04d907855fbd45091a6b1906946b8d4c
re1mond <re1mond@outlook.com>
2022-12-03 13:12:17 +0200

    Add controls

--- src/components/AssetsCard.vue
+++ src/components/AssetsCard.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="assets-card card">
     <div class="assets-card__list">
-      <div v-for="i in 5" class="assets-card__list-item coin-list-item">
+      <div
+        v-for="i in 5"
+        :key="i"
+        class="assets-card__list-item coin-list-item"
+      >
         <div class="coin-list-item__left">
           <div class="coin-list-item__logo"></div>
-          <div class="coin-list-item__label">TWT</div>
+          <div class="coin-list-item__details">
+            <div class="coin-list-item__label">TWT</div>
+            <div class="coin-list-item__price">$2,46</div>
+          </div>
         </div>
 
         <div class="coin-list-item__right">
-          <div class="coin-list-item__ammount">234</div>
-          <div class="coin-list-item__usd">~483$</div>
+          <div>
+            <div class="coin-list-item__ammount">234</div>
+            <div class="coin-list-item__usd">~483$</div>
+          </div>
+
+          <div class="coin-list-item__buttons">
+            <button
+              class="btn btn__content_text btn__bg_light-purple coin-list-item__button"
+            >
+              <i class="fa-solid fa-plus"></i>
+              Add
+            </button>
+            <button
+              class="btn btn__content_text btn__bg_purple coin-list-item__button"
+            >
+              <i class="fa-solid fa-trash"></i>
+              Remove
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -19,12 +43,21 @@
 </template>
 
 <script>
-import AddAssetBtn from "@/components/AddAssetBtn.vue"
+import AddAssetBtn from "@/components/AddAssetBtn.vue";
 export default {
-    components: {
-        AddAssetBtn
+  components: {
+    AddAssetBtn,
+  },
+
+  data() {
+    return {
+      tokenList: [
+        {
+          name: "TWT",
+          ammount: 100,
+        }
+      ]
     }
+  }
 };
 </script>
-
-
